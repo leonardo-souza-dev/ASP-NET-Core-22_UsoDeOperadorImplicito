e19753dc0de77a1aa88b18c01c82fe08a1957cb3
Leonardo Souza <leonardo.emerson-ext@viavarejo.com.br>
2019-08-30 18:20:34 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UsoDeOperadorImplicito
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Moeda libra = new Moeda { Sinal = "£", Valor = 100 };
+            Print(libra);
+
+            Moeda real = 200;
+            Print(real);
+
+            Moeda pmoeda = "P$";
+            Print(pmoeda);
+
+            Console.ReadKey();
+        }
+
+        public static void Print(Moeda m) =>
+            Console.WriteLine($"{m.Sinal} {m.Valor}");
+    }
+
+    public class Moeda
+    {
+        public string Sinal { get; set; }
+        public decimal Valor { get; set; }
+
+        public static implicit operator Moeda(decimal valor) =>
+            new Moeda { Sinal = "R$", Valor = valor };
+
+        public static implicit operator Moeda(string sinal) =>
+            new Moeda { Sinal = sinal, Valor = 1 };
+    }
+}
